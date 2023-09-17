59a6c67da8555f78d4f5afe46a2a7296905daac4
nakanoyuki <yukich41@gmail.com>
2023-09-17 23:20:41 +0900

    Lifting state up

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
 import { useState } from "react";
 import "./App.css";
 
-const Square = () => {
-  const [value, setValue] = useState<string | null>(null);
-  const handleClick = () => {
-    setValue("X");
-  };
+type SquareProps = {
+  value: string;
+};
 
+const Square = ({ value, onSquareClick }: SquareProps) => {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick} s>
       {value}
     </button>
   );
 };
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null)); // 長さが9ですべての要素が null で初期化された配列を作成
+  
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
