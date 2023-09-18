3c90141555d4db26ee176bbf25d482f31f3fa3aa
nakanoyuki <yukich41@gmail.com>
2023-09-18 10:26:12 +0900

    Add ClickEvent

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,46 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import "./App.css";
 
 type SquareProps = {
   value: string;
+  // Square コンポーネントが onSquareClick プロパティを受け取る
+  onSquareClick: () => void;
 };
 
 const Square = ({ value, onSquareClick }: SquareProps) => {
   return (
-    <button className="square" onClick={onSquareClick} s>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 };
 
+// すべてのマスの状態をボードコンポーネントに保持
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null)); // 長さが9ですべての要素が null で初期化された配列を作成
-  
+
+  const handleClick = (i: number) => {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  };
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        {/* イベントを表すプロップにはonSomethingという名前を使い、それらのイベントを処理する関数定義にはhandleSomethingを使うのが一般的 */}
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
