5fd872b993365a918b2e51aa085ef94eeae98403
nakanoyuki <yukich41@gmail.com>
2023-09-19 00:30:12 +0900

    Declaring a winner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { memo, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 type SquareProps = {
@@ -15,13 +15,34 @@ const Square = ({ value, onSquareClick }: SquareProps) => {
   );
 };
 
+const calculateWinner = (squares: (string | null)[]) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      return squares[a];
+  }
+  return null;
+};
 // すべてのマスの状態をボードコンポーネントに保持
 const Board = () => {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null)); // 長さが9ですべての要素が null で初期化された配列を作成
 
   const handleClick = (i: number) => {
-    if (squares[i]) return;
+    // calculateWinnerはsquares 配列に対して勝者がいるかどうかをチェックする関数
+    // ゲームに勝者がすでにいる場合、クリックを無視し、何もせずに関数を終了
+    if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -33,8 +54,17 @@ const Board = () => {
     // プレイヤーが移動するたびに、xIsNext（ブール値）が反転される
     setXIsNext(!xIsNext);
   };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         {/* イベントを表すプロップにはonSomethingという名前を使い、それらのイベントを処理する関数定義にはhandleSomethingを使うのが一般的 */}
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
